fe01847fd61a5b2eaa28ada87ea0ee17c849170c
jesussbayotgithub <jesussbayot@gmail.com>
2017-05-30 16:00:58 +0800

    Initial Upload of files

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+
+<div id="loader"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>
+
+<script type="text/javascript" src="/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="javascripts/script.js"></script>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include "includes/header.php"?>
+<h1>Hello, world!</h1>
+
+
+<?php include "includes/footer.php"?>
+

50aad732a5616cec3c1e0b60fa46bcf73d9903db
Jesus <jesussbayot@gmail.com>
2017-05-31 16:25:32 +0800

    Includes File

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,21 @@
+</div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                Footer
+            </div>
+        </div>
+    </div>
+</footer>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 
 <div id="loader"><div class="spinner"><div class="dot1"></div><div class="dot2"></div></div></div>
 
-<script type="text/javascript" src="/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="javascripts/script.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
 
 </body>
 </html>

0f86c1fb07a72ba1802851c11977036230a10449
Jesus <jesussbayot@gmail.com>
2017-05-31 16:26:04 +0800

    index.php

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php include "includes/header.php"?>
-<h1>Hello, world!</h1>
-
-
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12" style="height: 600px;">
+                <h1>Hello, world!</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+            </div>
+        </div>
+    </div>
+</section>
 <?php include "includes/footer.php"?>
 
